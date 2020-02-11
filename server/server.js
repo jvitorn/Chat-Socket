d8ae73bc31c5260a54ce893b43b9efa1620cb0d5
João Vitor Novais <joaoeuno2011@gmail.com>
2020-02-11 23:19:53 +0000

    definindo a primeira rota no server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//criando express
 const express = require('express');
 const path = require('path');
 //criando app 
@@ -8,3 +9,13 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 //definindo os arquivos publicos da aplicação 
 app.use(express.static(path.join(__dirname,'client')));
+//definindo onde ira ficar as views
+app.set('views',path.join(__dirname,'client'));
+//definindo a engine para 'html'
+app.engine('html',require('ejs').renderFile);
+//definindo a view
+app.set('view engine','html');
+//quando entrar na rota padrao sera iniciado o 'index.html'
+app.use('/',(req,res)=>{
+    res.render('index.html');
+});
