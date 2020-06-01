3f2555fa5aae1c888f76412f50249cd6700f82c6
João Vitor Novais <joaoeuno2011@gmail.com>
2020-06-01 14:01:32 +0000

    arrumando socket

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
-//definindo a porta do servidor
-const port = 3000;
-//criando express
+
 const express = require('express');
-const path = require('path');
-//criando app 
+//app express
 const app = express();
-//criando prototocolo  wss
-const server = require('http').createServer(app);
-//criando sockets
-const io = require('socket.io')(server);
-//definindo os arquivos publicos da aplicação 
-app.use(express.static(path.join(__dirname,'client')));
-//definindo onde ira ficar as views
-app.set('views',path.join(__dirname,'client'));
-//definindo a engine para 'html'
-app.engine('html',require('ejs').renderFile);
-//definindo a view
-app.set('view engine','html');
-//quando entrar na rota padrao sera iniciado o 'index.html'
-app.use('/',(req,res)=>{
-    res.render(submit);
-    res.render('index.html');
-});
-//iniciando conexao com um socket
-io.on('connection', socket =>{
-console.log('Socket Conectado: ${socket.id}');
-});
+const path = require('path');
 //server
-app.listen(port,function(){
-  console.log('Server is running at http://localhost:'+ port);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'client')));
+
+
+io.on('connection', (socket) => {
+console.log(`Socket conectado: ${socket.id}`)
 });
